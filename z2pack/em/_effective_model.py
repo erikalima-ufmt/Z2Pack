b694a6a9eb0fc98d2f4f7525ece1e1d1a2b346f6
Dominik Gresch <greschd@ethz.ch>
2015-10-02 10:31:27 +0200

    removed Timer in effective_model

--- z2pack/em/_effective_model.py
+++ z2pack/em/_effective_model.py
@@ -11,7 +11,7 @@ import numpy as np
 import scipy.linalg as la
 
 from .. import System as _Z2PackSystem
-from ptools.monitoring import Timer # DEBUG / OPTIMIZATION
+#~ from ptools.monitoring import Timer # DEBUG / OPTIMIZATION
 
 class System(_Z2PackSystem):
     r"""
@@ -95,19 +95,18 @@ class System(_Z2PackSystem):
         # create M - matrices
         # TODO: optimize
         M = []
-        with Timer('setting up M matrices'):
-            for i in range(0, N):
-                deltak = list(np.array(kpt[i + 1]) - np.array(kpt[i]))
-                dot_prod = [
-                    np.exp(-2j * np.pi * np.dot(deltak, self._pos[j]))
-                    for j in range(eigsize)
+        for i in range(0, N):
+            deltak = list(np.array(kpt[i + 1]) - np.array(kpt[i]))
+            dot_prod = [
+                np.exp(-2j * np.pi * np.dot(deltak, self._pos[j]))
+                for j in range(eigsize)
+            ]
+            Mnew = [[sum(
+                        np.conjugate(eigs[i][:, m]) * eigs[i + 1][:, n] * dot_prod
+                    )
+                    for n in range(eignum)
                 ]
-                Mnew = [[sum(
-                            np.conjugate(eigs[i][:, m]) * eigs[i + 1][:, n] * dot_prod
-                        )
-                        for n in range(eignum)
-                    ]
-                    for m in range(eignum)
-                ]
-                M.append(Mnew)
+                for m in range(eignum)
+            ]
+            M.append(Mnew)
         return M
